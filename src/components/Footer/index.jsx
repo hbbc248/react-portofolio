20c968dc04de6aaa5f3958163503a21c0aaffb30
Ibrahim <ibrahimzerlin@hotmail.com>
2021-06-24 20:20:39 -0500

    added some colors

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,7 +4,7 @@ const curYear = new Date().getFullYear();
 
 const Footer = () => (
   <>
-    <footer className="site-footer border-t-2 border-solid border-gray-400 py-2 mt-2">
+    <footer className="site-footer border-t-2 border-solid border-gray-500 py-2 mt-2">
       <div className="mx-auto container pt-2 lg:pt-2 flex flex-col items-center justify-center">
         <div className="text-black flex flex-col md:items-center f-f-l pt-3">
           <div className="md:flex items-center mt-2 md:mt-2 mb-2 text-base text-color f-f-l">
