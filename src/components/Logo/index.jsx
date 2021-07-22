bfa9a2791ba36651674c3311f9384cd5bfea1f84
Ibrahim <ibrahimzerlin@hotmail.com>
2021-07-21 19:10:06 -0500

    added link

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -3,7 +3,7 @@ import logo from "../../assets/images/my-portfolio-logo.png";
 
 const Logo = () => (
 	<div className="logo-container w-64">
-		<a href="/">
+		<a href="https://hbbc248.github.io/react-portofolio/">
 			<img src={logo} alt="BlueStormCreative logo" />
 		</a>
 	</div>
